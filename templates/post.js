eba1214f295f7352d44ef8ae4aa99039bb3ed7b6
Abdirrahman Mohamed <abdirrahmang@gmail.com>
2021-06-02 15:31:36 +0100

    some tailwindcss added

--- templates/post.js
+++ templates/post.js
@@ -3,20 +3,27 @@ import Link from 'next/link'
 
 
 
+import 'tailwindcss/tailwind.css'
 
 
 export default function Post({ children, frontMatter}) {
-    const { title} = frontMatter
+    const { title, date} = frontMatter
     return (
         <div>
             <Head>
                 <title>{title}</title>
             </Head>
-            <main>
-                <h1>{title}</h1>
-                <div>
+            <main className="font-sans flex items-center ">
+
+                <div className="max-w-2xl">
+                <h1 className="text-3xl">{title}</h1>
+                <h4 className="text-gray-500 text-sm ">{date}</h4>
+                <div className="font-body mt-5 ">
                     {children}
                 </div>
+                </div>
+
+               
                 <p>
                     <Link href="/">
                         <a>Back to Home</a>
